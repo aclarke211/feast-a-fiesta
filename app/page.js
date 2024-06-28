1fc91d86f6d64249547c8ea578c907b28b9c701f
Alex Clarke <aclarke211@gmail.com>
2024-06-28 22:32:49 +0100

    feat: :construction: add header and about page
    Create a Header component and an About page file. Also use the Link Next component to route to the new page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+import Header from '@/components/header';
+
 export default function Home() {
   return (
     <main>
-      <img src='/logo.png' alt='A server surrounded by magic sparkles.' />
-      <h1>Feast-a-Fiesta</h1>
+      <Header />
       <p>🥗🔥</p>
+      <p>
+        <Link href='/about'>About Us</Link>
+      </p>
     </main>
   );
 }
